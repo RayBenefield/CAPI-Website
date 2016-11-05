1c201264d3f16a6c48a8fa453a999fc4b3f83a89
Raymond Benefield <Raymond.Benefield@gmail.com>
2016-11-05 10:56:35 -0700

    Refactored into a Layout directory and shifted out the Header.

--- src/Layout/App.js
+++ src/Layout/App.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Header from './Header';
 import './App.css';
 
-const App = () => {
+export default () => {
     return (
         <div className="App">
-            <div className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h2>Welcome to React</h2>
-            </div>
+            <Header />
             <p className="App-intro">
                 To get started, edit <code>src/App.js</code> and save to reload.
             </p>
         </div>
     );
 };
-
-export default App;
