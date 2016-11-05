58965a334653302004f62303ab67858d9a65217d
Raymond Benefield <Raymond.Benefield@gmail.com>
2016-11-05 11:15:57 -0700

    Added a header test.

---
+++ src/Layout/Header.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from './Header';
+import renderer from 'react-test-renderer';
+
+describe('App Header', () => {
+    it('looks how we want it to look', () => {
+        const header = renderer.create(<Header />).toJSON();
+        expect(header).toMatchSnapshot();
+    });
+});
