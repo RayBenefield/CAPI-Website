b50fca80a7af70a3e90190f8ee6983caffed5700
Raymond Benefield <Raymond.Benefield@gmail.com>
2016-11-08 07:23:48 -0800

    Adjusted the layaout to handle 5 cards across.

--- src/Layout/Content/index.js
+++ src/Layout/Content/index.js
@@ -7,38 +7,43 @@ import { Responsive, WidthProvider } from 'react-grid-layout';
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
 
 const lgLayout = [
-    {i: 'a', x: 0, y: 0, w: 3, h: 10, static: true},
-    {i: 'b', x: 3, y: 0, w: 3, h: 10, static: true},
-    {i: 'c', x: 6, y: 0, w: 3, h: 10, static: true},
-    {i: 'd', x: 9, y: 0, w: 3, h: 10, static: true}
+    {i: 'a', x: 0, y: 0, w: 2, h: 10, static: true},
+    {i: 'b', x: 2, y: 0, w: 2, h: 10, static: true},
+    {i: 'c', x: 4, y: 0, w: 2, h: 10, static: true},
+    {i: 'd', x: 6, y: 0, w: 2, h: 10, static: true},
+    {i: 'e', x: 8, y: 0, w: 2, h: 10, static: true},
 ];
 
 const mdLayout = [
-    {i: 'a', x: 0, y: 0, w: 3, h: 10, static: true},
-    {i: 'b', x: 3, y: 0, w: 3, h: 10, static: true},
-    {i: 'c', x: 6, y: 0, w: 3, h: 10, static: true},
-    {i: 'd', x: 0, y: 10, w: 3, h: 10, static: true}
+    {i: 'a', x: 0, y: 0, w: 2, h: 10, static: true},
+    {i: 'b', x: 2, y: 0, w: 2, h: 10, static: true},
+    {i: 'c', x: 4, y: 0, w: 2, h: 10, static: true},
+    {i: 'd', x: 6, y: 0, w: 2, h: 10, static: true},
+    {i: 'e', x: 0, y: 10, w: 2, h: 10, static: true},
 ];
 
 const smLayout = [
-    {i: 'a', x: 0, y: 0, w: 3, h: 10, static: true},
-    {i: 'b', x: 3, y: 0, w: 3, h: 10, static: true},
-    {i: 'c', x: 0, y: 10, w: 3, h: 10, static: true},
-    {i: 'd', x: 3, y: 10, w: 3, h: 10, static: true}
+    {i: 'a', x: 0, y: 0, w: 2, h: 10, static: true},
+    {i: 'b', x: 2, y: 0, w: 2, h: 10, static: true},
+    {i: 'c', x: 4, y: 0, w: 2, h: 10, static: true},
+    {i: 'd', x: 0, y: 10, w: 2, h: 10, static: true},
+    {i: 'e', x: 2, y: 10, w: 2, h: 10, static: true},
 ];
 
 const xsLayout = [
-    {i: 'a', x: 0, y: 0, w: 3, h: 10, static: true},
-    {i: 'b', x: 0, y: 10, w: 3, h: 10, static: true},
-    {i: 'c', x: 0, y: 20, w: 3, h: 10, static: true},
-    {i: 'd', x: 0, y: 30, w: 3, h: 10, static: true}
+    {i: 'a', x: 0, y: 0, w: 2, h: 10, static: true},
+    {i: 'b', x: 2, y: 0, w: 2, h: 10, static: true},
+    {i: 'c', x: 0, y: 10, w: 2, h: 10, static: true},
+    {i: 'd', x: 2, y: 10, w: 2, h: 10, static: true},
+    {i: 'e', x: 0, y: 20, w: 2, h: 10, static: true},
 ];
 
 const xxsLayout = [
-    {i: 'a', x: 0, y: 0, w: 3, h: 10, static: true},
-    {i: 'b', x: 0, y: 10, w: 3, h: 10, static: true},
-    {i: 'c', x: 0, y: 20, w: 3, h: 10, static: true},
-    {i: 'd', x: 0, y: 30, w: 3, h: 10, static: true}
+    {i: 'a', x: 0, y: 0, w: 2, h: 10, static: true},
+    {i: 'b', x: 0, y: 10, w: 2, h: 10, static: true},
+    {i: 'c', x: 0, y: 20, w: 2, h: 10, static: true},
+    {i: 'd', x: 0, y: 30, w: 2, h: 10, static: true},
+    {i: 'e', x: 0, y: 40, w: 2, h: 10, static: true},
 ];
 
 const layouts = {
@@ -51,8 +56,8 @@ const layouts = {
 
 const Content = () => (
     <ResponsiveReactGridLayout className="layout" autoSize={false} layouts={layouts}
-        breakpoints={{lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0}}
-        cols={{lg: 12, md: 9, sm: 6, xs: 3, xxs: 3}}
+        breakpoints={{lg: 1800, md: 1350, sm: 900, xs: 450, xxs: 0}}
+        cols={{lg: 10, md: 8, sm: 6, xs: 4, xxs: 2}}
         rowHeight={28}>
         <div key={'a'}>
             <Card className="card">
@@ -130,6 +135,31 @@ const Content = () => (
             </Card>
         </div>
         <div key={'d'}>
+            <Card className="card">
+                <CardHeader
+                    title="URL Avatar"
+                    subtitle="Subtitle"
+                    avatar="images/jsa-128.jpg"
+                />
+                <CardMedia
+                    //overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+                >
+                    <img src='images/nature-600-337.jpg' alt='nature'/>
+                </CardMedia>
+                <CardTitle title="Card title" subtitle="Card subtitle" />
+                <CardText>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+                    Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+                    Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+                </CardText>
+                <CardActions>
+                    <FlatButton label="Action1" />
+                    <FlatButton label="Action2" />
+                </CardActions>
+            </Card>
+        </div>
+        <div key={'e'}>
             <Card className="card">
                 <CardHeader
                     title="URL Avatar"
