ed303d3a8142b09e7789006c60bcdad26af695b2
Raymond Benefield <Raymond.Benefield@gmail.com>
2016-11-12 08:16:18 -0800

    Made the cards static.

--- src/Layout/Grid/layouts.js
+++ src/Layout/Grid/layouts.js
@@ -1,41 +1,41 @@
 const lgLayout = [
-    {i: 'a', x: 0, y: 0, w: 2, h: 7},
-    {i: 'b', x: 2, y: 0, w: 2, h: 7},
-    {i: 'c', x: 4, y: 0, w: 2, h: 7},
-    {i: 'd', x: 6, y: 0, w: 2, h: 7},
-    {i: 'e', x: 8, y: 0, w: 2, h: 7},
+    {i: 'a', x: 0, y: 0, w: 2, h: 7, static: true},
+    {i: 'b', x: 2, y: 0, w: 2, h: 7, static: true},
+    {i: 'c', x: 4, y: 0, w: 2, h: 7, static: true},
+    {i: 'd', x: 6, y: 0, w: 2, h: 7, static: true},
+    {i: 'e', x: 8, y: 0, w: 2, h: 7, static: true},
 ];
 
 const mdLayout = [
-    {i: 'a', x: 0, y: 0, w: 2, h: 7},
-    {i: 'b', x: 2, y: 0, w: 2, h: 7},
-    {i: 'c', x: 4, y: 0, w: 2, h: 7},
-    {i: 'd', x: 6, y: 0, w: 2, h: 7},
-    {i: 'e', x: 0, y: 10, w: 2, h: 7},
+    {i: 'a', x: 0, y: 0, w: 2, h: 7, static: true},
+    {i: 'b', x: 2, y: 0, w: 2, h: 7, static: true},
+    {i: 'c', x: 4, y: 0, w: 2, h: 7, static: true},
+    {i: 'd', x: 6, y: 0, w: 2, h: 7, static: true},
+    {i: 'e', x: 0, y: 10, w: 2, h: 7, static: true},
 ];
 
 const smLayout = [
-    {i: 'a', x: 0, y: 0, w: 2, h: 7},
-    {i: 'b', x: 2, y: 0, w: 2, h: 7},
-    {i: 'c', x: 4, y: 0, w: 2, h: 7},
-    {i: 'd', x: 0, y: 10, w: 2, h: 7},
-    {i: 'e', x: 2, y: 10, w: 2, h: 7},
+    {i: 'a', x: 0, y: 0, w: 2, h: 7, static: true},
+    {i: 'b', x: 2, y: 0, w: 2, h: 7, static: true},
+    {i: 'c', x: 4, y: 0, w: 2, h: 7, static: true},
+    {i: 'd', x: 0, y: 10, w: 2, h: 7, static: true},
+    {i: 'e', x: 2, y: 10, w: 2, h: 7, static: true},
 ];
 
 const xsLayout = [
-    {i: 'a', x: 0, y: 0, w: 2, h: 7},
-    {i: 'b', x: 2, y: 0, w: 2, h: 7},
-    {i: 'c', x: 0, y: 10, w: 2, h: 7},
-    {i: 'd', x: 2, y: 10, w: 2, h: 7},
-    {i: 'e', x: 0, y: 20, w: 2, h: 7},
+    {i: 'a', x: 0, y: 0, w: 2, h: 7, static: true},
+    {i: 'b', x: 2, y: 0, w: 2, h: 7, static: true},
+    {i: 'c', x: 0, y: 10, w: 2, h: 7, static: true},
+    {i: 'd', x: 2, y: 10, w: 2, h: 7, static: true},
+    {i: 'e', x: 0, y: 20, w: 2, h: 7, static: true},
 ];
 
 const xxsLayout = [
-    {i: 'a', x: 0, y: 0, w: 2, h: 7},
-    {i: 'b', x: 0, y: 10, w: 2, h: 7},
-    {i: 'c', x: 0, y: 20, w: 2, h: 7},
-    {i: 'd', x: 0, y: 30, w: 2, h: 7},
-    {i: 'e', x: 0, y: 40, w: 2, h: 7},
+    {i: 'a', x: 0, y: 0, w: 2, h: 7, static: true},
+    {i: 'b', x: 0, y: 10, w: 2, h: 7, static: true},
+    {i: 'c', x: 0, y: 20, w: 2, h: 7, static: true},
+    {i: 'd', x: 0, y: 30, w: 2, h: 7, static: true},
+    {i: 'e', x: 0, y: 40, w: 2, h: 7, static: true},
 ];
 
 const layouts = {
