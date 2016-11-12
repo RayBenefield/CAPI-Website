202194fadea51a79b05d63c0ca1b132955659517
Raymond Benefield <Raymond.Benefield@gmail.com>
2016-11-12 08:19:55 -0800

    Set proper height on everything.

--- src/Layout/Grid/layouts.js
+++ src/Layout/Grid/layouts.js
@@ -11,31 +11,31 @@ const mdLayout = [
     {i: 'b', x: 2, y: 0, w: 2, h: 7, static: true},
     {i: 'c', x: 4, y: 0, w: 2, h: 7, static: true},
     {i: 'd', x: 6, y: 0, w: 2, h: 7, static: true},
-    {i: 'e', x: 0, y: 10, w: 2, h: 7, static: true},
+    {i: 'e', x: 0, y: 7, w: 2, h: 7, static: true},
 ];
 
 const smLayout = [
     {i: 'a', x: 0, y: 0, w: 2, h: 7, static: true},
     {i: 'b', x: 2, y: 0, w: 2, h: 7, static: true},
     {i: 'c', x: 4, y: 0, w: 2, h: 7, static: true},
-    {i: 'd', x: 0, y: 10, w: 2, h: 7, static: true},
-    {i: 'e', x: 2, y: 10, w: 2, h: 7, static: true},
+    {i: 'd', x: 0, y: 7, w: 2, h: 7, static: true},
+    {i: 'e', x: 2, y: 7, w: 2, h: 7, static: true},
 ];
 
 const xsLayout = [
     {i: 'a', x: 0, y: 0, w: 2, h: 7, static: true},
     {i: 'b', x: 2, y: 0, w: 2, h: 7, static: true},
-    {i: 'c', x: 0, y: 10, w: 2, h: 7, static: true},
-    {i: 'd', x: 2, y: 10, w: 2, h: 7, static: true},
-    {i: 'e', x: 0, y: 20, w: 2, h: 7, static: true},
+    {i: 'c', x: 0, y: 7, w: 2, h: 7, static: true},
+    {i: 'd', x: 2, y: 7, w: 2, h: 7, static: true},
+    {i: 'e', x: 0, y: 14, w: 2, h: 7, static: true},
 ];
 
 const xxsLayout = [
     {i: 'a', x: 0, y: 0, w: 2, h: 7, static: true},
-    {i: 'b', x: 0, y: 10, w: 2, h: 7, static: true},
-    {i: 'c', x: 0, y: 20, w: 2, h: 7, static: true},
-    {i: 'd', x: 0, y: 30, w: 2, h: 7, static: true},
-    {i: 'e', x: 0, y: 40, w: 2, h: 7, static: true},
+    {i: 'b', x: 0, y: 7, w: 2, h: 7, static: true},
+    {i: 'c', x: 0, y: 14, w: 2, h: 7, static: true},
+    {i: 'd', x: 0, y: 21, w: 2, h: 7, static: true},
+    {i: 'e', x: 0, y: 28, w: 2, h: 7, static: true},
 ];
 
 const layouts = {
