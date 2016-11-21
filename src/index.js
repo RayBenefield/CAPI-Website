adb23177a5c0e24c45a18dfe7ee1009896b1cc46
Raymond Benefield <Raymond.Benefield@gmail.com>
2016-11-21 06:39:47 -0800

    Moved the absolute imports above the relative ones.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Layout/App';
-import './index.css';
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
+import App from './Layout/App';
+import './index.css';
 
 ReactDOM.render(
     <App />,
