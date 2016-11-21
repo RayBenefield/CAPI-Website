b8006426bd346c00da951ad04ae851d729ffde3d
Raymond Benefield <Raymond.Benefield@gmail.com>
2016-11-21 06:55:52 -0800

    Added Provider and store for redux.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './Layout/App';
 import './index.css';
 
+const store = createStore((state) => state);
+
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
