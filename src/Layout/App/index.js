4175664117012f7c55ee2a7967dce81223aab155
Raymond Benefield <Raymond.Benefield@gmail.com>
2016-11-21 07:45:28 -0800

    Refactored the App into its own folder.

--- src/Layout/App/index.js
+++ src/Layout/App/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import Header from './Header';
-import ContentContainer from './ContentContainer';
-import './App.css';
+import Header from '../Header';
+import ContentContainer from '../ContentContainer';
+import './index.css';
 
 darkBaseTheme.palette.backgroundColor = '#424242';
 const App = () => (
