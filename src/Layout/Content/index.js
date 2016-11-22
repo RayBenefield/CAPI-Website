6c101cc33868ae68bdf17572f4beaa456ce61a9f
Raymond Benefield <Raymond.Benefield@gmail.com>
2016-11-22 07:26:53 -0800

    Derived item height based on image width.

--- src/Layout/Content/index.js
+++ src/Layout/Content/index.js
@@ -3,13 +3,14 @@ import FileGrid from '../StaticResponsiveGrid';
 import FileCard from '../FileCard';
 
 const Content = ({files}) => {
+    const imageHeight = 320;
     const fileCards = files
         .map((file, id) => {
             return <FileCard key={id.toString()} file={file} />;
         });
 
     return (
-        <FileGrid itemWidth={320} itemHeight={169} maxWidth={1600} items={fileCards} />
+        <FileGrid itemWidth={imageHeight} itemHeight={imageHeight * 0.5635179153} maxWidth={1600} items={fileCards} />
     );
 }
 
