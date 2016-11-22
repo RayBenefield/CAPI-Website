b3648074a15884b39718a86958abe09241a280f3
Raymond Benefield <Raymond.Benefield@gmail.com>
2016-11-21 16:55:58 -0800

    Refactored how to handle containers.

--- src/Layout/App/index.js
+++ src/Layout/App/index.js
@@ -3,7 +3,7 @@ import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import Header from '../Header';
-import ContentContainer from '../ContentContainer';
+import Content from '../Content/Container';
 import './index.css';
 
 darkBaseTheme.palette.backgroundColor = '#424242';
@@ -11,7 +11,7 @@ const App = () => (
     <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
         <div className="App">
             <Header />
-            <ContentContainer />
+            <Content />
         </div>
     </MuiThemeProvider>
 );
