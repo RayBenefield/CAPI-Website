3b10c8d5ec74b62a4b4dfec72d715f5791d69606
Raymond Benefield <Raymond.Benefield@gmail.com>
2016-11-26 06:47:48 -0800

    [Finished #134935081] Added hover effects on the File Cards.

--- src/Layout/FileCard/index.js
+++ src/Layout/FileCard/index.js
@@ -3,20 +3,32 @@ import { CardTitle, CardMedia } from 'material-ui/Card';
 import Paper from 'material-ui/Paper';
 import './index.css';
 
-const FileCard = ({ className, style, file }) => {
-    return (
-        <div className={className} style={style}>
-            <Paper className="card" zDepth={2}>
-                <CardMedia
-                    overlay={
-                        <CardTitle title={file.name} titleStyle={{ fontSize: '20px', lineHeight: '12px' }} />
-                    }
-                >
-                    <img src={file.image} alt='nature' />
-                </CardMedia>
-            </Paper>
-        </div>
-    );
-};
+const FileCard = React.createClass({
+    getInitialState() {
+        return { depth: 2 };
+    },
+    onHover() {
+        this.setState({ depth: 4 });
+    },
+    noHover() {
+        this.setState({ depth: 2 });
+    },
+    render() {
+        const { className, style, file } = this.props;
+        return (
+            <div className={className} style={style}>
+                <Paper className="card" zDepth={this.state.depth} onMouseEnter={this.onHover} onMouseLeave={this.noHover} >
+                    <CardMedia
+                        overlay={
+                            <CardTitle title={file.name} titleStyle={{ fontSize: '20px', lineHeight: '12px' }} />
+                        }
+                    >
+                        <img src={file.image} alt='nature' />
+                    </CardMedia>
+                </Paper>
+            </div>
+        );
+    }
+});
 
 export default FileCard;
